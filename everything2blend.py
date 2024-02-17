07f354140fd8f904a85fc5da85d93d224c24eebf
rozab <roryabarnett@gmail.com>
2024-02-17 05:07:18 +0000

    make everything2blend arguments saner

--- everything2blend.py
+++ everything2blend.py
@@ -11,40 +11,51 @@ try:
 except ImportError:
     print("This script requires pillow to work. Run pip install pillow!")
 
+DEFAULT_BLENDER_LOCATION = r"C:\Program Files\Blender Foundation\Blender 3.5\blender.exe"
+DEFAULT_CS_LOCATION = r"C:\Program Files (x86)\Microsoft Games\Crimson Skies"
+
 BLENDER_ARGS = ["--background", "--factory-startup", "--python-use-system-env", "--python"]
 CHAPTERS = ["c1", "c1b", "c1c", "c2", "c2b", "c3", "c4", "c5"]
 
 parser = argparse.ArgumentParser(description="Convert dumped Crimson Skies plane model data to blender files.")
 parser.add_argument(
-    "blender_exe",
-    type=lambda value: Path(value).resolve(strict=True),
-    help="Path to your Blender executable")
-parser.add_argument(
-    "--zips",
-    metavar="FOLDER",
-    dest="data_folder",
-    default=".",
-    type=lambda value: Path(value).resolve(strict=True),
-    help="Folder containing planes.zip and textures.zip")
-parser.add_argument(
-    "--out",
-    metavar="FOLDER",
-    dest="out_folder",
-    default=".",
-    type=lambda value: Path(value).resolve(strict=True),
-    help="Folder to save .blend files")
-parser.add_argument(
-    "--zbd",
-    metavar="FOLDER",
-    dest="zbd",
-    type=lambda value: Path(value).resolve(strict=True),
-    help="Path to ZBD folder in game files")
-parser.add_argument(
     "--unzbd",
     metavar="EXE",
     dest="unzbd",
     type=lambda value: Path(value).resolve(strict=True),
-    help="Path to unzbd executable (will automate extraction if supplied). Requires --zbd")
+    help="Path to unzbd executable")
+parser.add_argument(
+    "--blender",
+    metavar="EXE",
+    dest="blender",
+    default=DEFAULT_BLENDER_LOCATION,
+    type=lambda value: Path(value).resolve(strict=True),
+    help="Path to your Blender executable")
+parser.add_argument(
+    "--cs",
+    metavar="FOLDER",
+    dest="cs",
+    default=DEFAULT_CS_LOCATION,
+    type=lambda value: Path(value).resolve(strict=True),
+    help="Path to CS install folder")
+parser.add_argument(
+    "--data",
+    metavar="FOLDER",
+    dest="data_folder",
+    default=Path("unzbd_output"),
+    type=lambda value: Path(value).resolve(strict=True),
+    help="Folder for intermediate data")
+parser.add_argument(
+    "--out",
+    metavar="FOLDER",
+    dest="blend_folder",
+    default=Path("blend_output"),
+    type=lambda value: Path(value).resolve(strict=True),
+    help="Folder to save .blend files")
+parser.add_argument(
+    "--skip-unzbd",
+    action="store_true",
+    help="Use existing unzbd output")
 parser.add_argument(
     "--skip-planes",
     action="store_true",
@@ -60,46 +71,36 @@ except FileNotFoundError as e:
     print(e)
     exit(1)
 
-if args.unzbd:
-    if not args.zbd:
-        print("ZBD folder must be specified when using --unzbd")
-        exit(1)
 
-    print("Extracting .cab files...")
-    with TemporaryDirectory(dir=".") as tempdir:
-        tempdir = Path(tempdir)
-        for c in CHAPTERS:
-            try:
-                (tempdir / c).mkdir()
-                sub.Popen(["expand.exe", "-F:*", str(args.zbd / f"{c}.CAB"), str(tempdir / c)], stdout=sub.PIPE, stderr=sub.PIPE)
-            except:
-                print("Couldn't extract .CABs with expand.exe")
-                exit(1)
-        
-        time.sleep(1) # expand.exe is so fucked
+args.data_folder.mkdir(exist_ok=True)
+args.blend_folder.mkdir(exist_ok=True)
 
-        print("Extracting .zbd files...")
-        for c in CHAPTERS:
-            gamez_path = next((tempdir / c).rglob("gamez.zbd"))
-            sub.Popen([args.unzbd, "cs", "gamez", str(gamez_path), str(args.data_folder / f"{c}.zip")]).communicate()
+if not args.unzbd:
+    print("Must specify unzbd.exe location with --unzbd")
+    exit(1)
 
-        # dump all textures in same .zip
-        (tempdir / "textures").mkdir()
-        for f in tempdir.rglob("*textu*.zbd"):
-            sub.Popen([args.unzbd, "cs", "textures", f, str(tempdir / "temp.zip")]).communicate()
-            with ZipFile(tempdir / "temp.zip") as z:
-                z.extractall(tempdir / "textures")
-            (tempdir / "temp.zip").unlink()
+if not args.skip_unzbd:
+    print("Extracting planes.zbd...")
+    sub.Popen([args.unzbd, "cs", "gamez", str(args.cs / "ZBD" / "PLANES.ZBD"), str(args.data_folder / "planes.zip")]).communicate()
 
-        with ZipFile(args.data_folder / "textures.zip", "w") as z:
-            for f in (tempdir / "textures").glob("*.png"):
-                z.write(f, arcname = f.name)
+    print("Extracting gamez.zbd...")
+    for c in CHAPTERS:
+        gamez_path = args.cs / "ZBD" / c / "gamez.zbd"
+        sub.Popen([args.unzbd, "cs", "gamez", str(gamez_path), str(args.data_folder / f"{c}.zip")]).communicate()
+
+    print("Extracting texture.zbd...")
+    with ZipFile(args.data_folder / "textures.zip", "w") as agg_zip:
+        for f in args.cs.rglob("texture.zbd"):
+            zip_path = args.data_folder / f"{f.parent.name}_textures.zip"
+            sub.Popen([args.unzbd, "cs", "textures", f, str(zip_path)]).communicate()
+            with ZipFile(zip_path) as chapter_zip:
+                for texture_name in chapter_zip.namelist():
+                    if texture_name not in agg_zip.namelist():
+                        agg_zip.writestr(texture_name, chapter_zip.read(texture_name))
     
-    sub.Popen([args.unzbd, "cs", "gamez", str(args.zbd / "PLANES.ZBD"), str(args.data_folder / "planes.zip")]).communicate()
 
 if not args.skip_planes:
     print("Generating plane .blends...")
-    print("Searching for root nodes...")
 
     with ZipFile(str(args.data_folder / "planes.zip")) as level:
         with level.open("nodes.json") as f:
@@ -109,16 +110,15 @@ if not args.skip_planes:
     for i, n in enumerate(nodes_json):
         v = next(iter(n.values()))
         if v["parent"] is None:
-            print(f"Found root node with name {v['name']} at index {i}")
             roots.append((i, v["name"]))
 
     for i, name in roots:
         print(f"Generating {name}.blend...")
-        sub.Popen([args.blender_exe] + BLENDER_ARGS + ["plane2blend.py", "--", str(args.data_folder), str(args.out_folder), str(i)]).communicate()
+        sub.Popen([args.blender] + BLENDER_ARGS + ["plane2blend.py", "--", str(args.data_folder), str(args.blend_folder.resolve()), str(i)]).communicate()
 
 if not args.skip_levels:
     print("Generating level .blends...")
 
     for c in CHAPTERS:
         print(f"Generating {c}.blend...")
-        sub.Popen([args.blender_exe] + BLENDER_ARGS + ["world2blend.py", "--", str(args.data_folder), str(args.out_folder), c]).communicate()
+        sub.Popen([args.blender] + BLENDER_ARGS + ["world2blend.py", "--", str(args.data_folder), str(args.blend_folder.resolve()), c]).communicate()
